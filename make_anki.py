761cb0efbb52dbcde31eac652f2215e55fbaf675
Markus Amano <markus.a.amano@gmail.com>
2024-01-23 09:49:54 +0900

    not including header

--- make_anki.py
+++ make_anki.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sys
 import os
 
-# id,japanese,english,particle,word,kanji,furigana,obi2
+# initial fields: id,japanese,english,particle,word,kanji,furigana,obi2
 
 
 def obi_str_to_num(sobi: str) -> int:
@@ -14,17 +14,15 @@ def main():
         print(f"usage: {sys.argv[0]} <inputfile.csv>")
         quit()
     sents = pd.read_csv(sys.argv[1])
-    # print(sents)
     col = sents["obi2"]
     sents["obi2_int"] = col.map(obi_str_to_num)
-    # print(sents)
     sents.sort_values("obi2_int", inplace=True)
     sents['anki_tags'] = 'id::' + sents['id'].astype(str)\
         + ',particle::' + sents['particle']\
         + ',word::' + sents['word']\
         + ',kanji::' + sents['kanji']\
         + ',obi::' + sents['obi2_int'].astype(str)
-    sents.to_csv("anki_kaiwa.csv", index=False)
+    sents.to_csv("anki_kaiwa.csv", index=False, header=False)
 
 
 if __name__ == "__main__":
